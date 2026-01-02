c990b2e05e2bc07608060d3966b7fa1df8921230
mystster <mystster@gmail.com>
2026-01-03 00:06:45 +0900

    fix(setup): use robust sed address delimiter in cleanup_tools

--- setup.fish
+++ setup.fish
@@ -82,8 +82,7 @@ function cleanup_tools
         # Remove from .gitignore if exists
         if test -f "$HOME/.gitignore"
             # Remove the exact line "!$tool"
-            set -l escaped_tool (echo $tool | sed 's/\//\\\//g')
-            sed -i "/^!$escaped_tool\$/d" "$HOME/.gitignore"
+            sed -i "\|^!$tool\$|d" "$HOME/.gitignore"
         end
     end
 end
