3b6c43838c724401315bdb90b1516bc12ec51033
mystster <mystster@gmail.com>
2026-02-26 23:12:47 +0900

    feat: add dot-commit-ai custom command of lazygit

--- .config/fish/functions/dot-lazy.fish
+++ .config/fish/functions/dot-lazy.fish
@@ -1,6 +1,7 @@
 function dot-lazy --description 'Lazygit wrapper for dotfiles'
     # Default to $HOME/.cfg if not set
     set -q DOTFILES_DIR; or set -Ux DOTFILES_DIR $HOME/.dotfiles.git
+    set lazy_config (realpath (string join (status dirname) 'dot-lazy.yml'))
 
-    lazygit --git-dir=$DOTFILES_DIR --work-tree=$HOME $argv
+    lazygit --git-dir=$DOTFILES_DIR --work-tree=$HOME $argv -ucf $lazy_config
 end
